3cd2684b5f94feab79cd8ab859fc1e3818278d36
orcaman <orhiltch@gmail.com>
2019-08-01 18:50:20 +0300

    remove ctor

--- rediss.py
+++ rediss.py
@@ -7,20 +7,6 @@ class RedisLogger():
     password = os.getenv('REDIS_PASSWORD')
     r = redis.Redis(host=host, port=port, password=password)
 
-    def __init__(self, host=os.getenv('REDIS_HOST'), port=os.getenv('REDIS_PORT'), password=os.getenv('REDIS_PASSWORD')):
-        reinit = False
-        if host is not None:
-            self.host = host
-            reinit = True
-        if port is not None:
-            self.port = port
-            reinit = True
-        if password is not None:
-            self.password = password
-            reinit = True
-        if reinit is True:
-            self.r = redis.Redis(host=host, port=port, password=password)
-
     def set(self, k, v): 
         self.r.set(k, v)
 
